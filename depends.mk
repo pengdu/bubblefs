8edfb62672e125b5ce79953015bf23dc902ea705
mengjiahao <691225916@qq.com>
2017-09-25 20:37:52 +0800

    change the order in depends.mk

--- depends.mk
+++ depends.mk
@@ -4,15 +4,15 @@
 ########################################
 
 BOOST_PATH=$(PROJECT_DIR)/third_party/boost
-JEMALLOC_PATH=$(PROJECT_DIR)/third_party
-RAPIDJSON_PATH=$(PROJECT_DIR)/third_party
 CPP_BTREE_PATH=$(PROJECT_DIR)/third_party
+GPERFTOOLS_PATH=$(PROJECT_DIR)/thirdparty
 GTEST_PATH=$(PROJECT_DIR)/third_party
-SNAPPY_PATH=$(PROJECT_DIR)/third_party
+JEMALLOC_PATH=$(PROJECT_DIR)/third_party
 LEVELDB_PATH=$(PROJECT_DIR)/third_party
 PROTOBUF_PATH=$(PROJECT_DIR)/third_party
 PROTOC_PATH=$(PROJECT_DIR)/third_party/bin
 PROTOC=$(PROTOC_PATH)/protoc
+RAPIDJSON_PATH=$(PROJECT_DIR)/third_party
+SNAPPY_PATH=$(PROJECT_DIR)/third_party
 SOFA_PBRPC_PATH=$(PROJECT_DIR)/third_party
-GPERFTOOLS_PATH=$(PROJECT_DIR)/thirdparty
 
