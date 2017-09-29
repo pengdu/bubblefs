86d1a0f22e75fb07e49775a2b524f50efc4696d0
mengjiahao <691225916@qq.com>
2017-09-29 17:31:08 +0800

    add endpoint

--- src/utils/hash.h
+++ src/utils/hash.h
@@ -29,76 +29,6 @@ limitations under the License.
 
 namespace bubblefs {
   
-extern uint32 Hash32(const char* data, size_t n, uint32 seed);
-extern uint64 Hash64(const char* data, size_t n, uint64 seed);
-
-inline uint64 Hash64(const char* data, size_t n) {
-  return Hash64(data, n, 0xDECAFCAFFE);
-}
-
-inline uint64 Hash64(const string& str) {
-  return Hash64(str.data(), str.size());
-}
-
-inline uint64 Hash64Combine(uint64 a, uint64 b) {
-  return a ^ (b + 0x9e3779b97f4a7800ULL + (a << 10) + (a >> 4));
-}
-
-inline uint32_t BloomHash(const StringPiece& key) {
-  return Hash32(key.data(), key.size(), 0xbc9f1d34);
-}
-
-inline uint32_t GetStringPieceHash(const StringPiece& s) {
-  return Hash32(s.data(), s.size(), 397);
-}
-
-// std::hash compatible interface.
-// std::unordered_map<StringPiece, void*, StringPieceHasher> insert_hints_;
-struct StringPieceHasher {
-  uint32_t operator()(const StringPiece& s) const { return GetStringPieceHash(s); }
-};
-
-// Hash functor suitable for use with power-of-two sized hashtables.  Use
-// instead of std::hash<T>.
-//
-// In particular, tensorflow::hash is not the identity function for pointers.
-// This is important for power-of-two sized hashtables like FlatMap and FlatSet,
-// because otherwise they waste the majority of their hash buckets.
-template <typename T>
-struct hash {
-  size_t operator()(const T& t) const { return std::hash<T>()(t); }
-};
-
-template <typename T>
-struct hash<T*> {
-  size_t operator()(const T* t) const {
-    // Hash pointers as integers, but bring more entropy to the lower bits.
-    size_t k = static_cast<size_t>(reinterpret_cast<uintptr_t>(t));
-    return k + (k >> 6);
-  }
-};
-
-template <>
-struct hash<string> {
-  size_t operator()(const string& s) const {
-    return static_cast<size_t>(Hash64(s));
-  }
-};
-
-template <>
-struct hash<StringPiece> {
-  size_t operator()(StringPiece sp) const {
-    return static_cast<size_t>(Hash64(sp.data(), sp.size()));
-  }
-};
-
-template <typename T, typename U>
-struct hash<std::pair<T, U>> {
-  size_t operator()(const std::pair<T, U>& p) const {
-    return Hash64Combine(hash<T>()(p.first), hash<U>()(p.second));
-  }
-};
-
 // Robert Jenkins' function for mixing 32-bit values
 // http://burtleburtle.net/bob/hash/evahash.html
 // a, b = random bits, c = input and output
@@ -179,6 +109,78 @@ public:
       p++;
     }
     return H(acc);
+  }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////
+  
+extern uint32 Hash32(const char* data, size_t n, uint32 seed);
+extern uint64 Hash64(const char* data, size_t n, uint64 seed);
+
+inline uint64 Hash64(const char* data, size_t n) {
+  return Hash64(data, n, 0xDECAFCAFFE);
+}
+
+inline uint64 Hash64(const string& str) {
+  return Hash64(str.data(), str.size());
+}
+
+inline uint64 Hash64Combine(uint64 a, uint64 b) {
+  return a ^ (b + 0x9e3779b97f4a7800ULL + (a << 10) + (a >> 4));
+}
+
+inline uint32_t BloomHash(const StringPiece& key) {
+  return Hash32(key.data(), key.size(), 0xbc9f1d34);
+}
+
+inline uint32_t GetStringPieceHash(const StringPiece& s) {
+  return Hash32(s.data(), s.size(), 397);
+}
+
+// std::hash compatible interface.
+// std::unordered_map<StringPiece, void*, StringPieceHasher> insert_hints_;
+struct StringPieceHasher {
+  uint32_t operator()(const StringPiece& s) const { return GetStringPieceHash(s); }
+};
+
+// Hash functor suitable for use with power-of-two sized hashtables.  Use
+// instead of std::hash<T>.
+//
+// In particular, tensorflow::hash is not the identity function for pointers.
+// This is important for power-of-two sized hashtables like FlatMap and FlatSet,
+// because otherwise they waste the majority of their hash buckets.
+template <typename T>
+struct hash {
+  size_t operator()(const T& t) const { return std::hash<T>()(t); }
+};
+
+template <typename T>
+struct hash<T*> {
+  size_t operator()(const T* t) const {
+    // Hash pointers as integers, but bring more entropy to the lower bits.
+    size_t k = static_cast<size_t>(reinterpret_cast<uintptr_t>(t));
+    return k + (k >> 6);
+  }
+};
+
+template <>
+struct hash<string> {
+  size_t operator()(const string& s) const {
+    return static_cast<size_t>(Hash64(s));
+  }
+};
+
+template <>
+struct hash<StringPiece> {
+  size_t operator()(StringPiece sp) const {
+    return static_cast<size_t>(Hash64(sp.data(), sp.size()));
+  }
+};
+
+template <typename T, typename U>
+struct hash<std::pair<T, U>> {
+  size_t operator()(const std::pair<T, U>& p) const {
+    return Hash64Combine(hash<T>()(p.first), hash<U>()(p.second));
   }
 };
 
