48eb580bfb4199c57e2f615831f0a2e04091548b
mengjiahao <691225916@qq.com>
2017-10-20 00:36:13 +0800

    add git/compressed_trie

--- src/utils/git_trie.cc
+++ src/utils/git_trie.cc
@@ -187,6 +187,22 @@ static struct common_dir common_list[] = {
         { 0, 0, 0, "shallow" },
         { 0, 0, 0, NULL }
 };
+
+static struct trie common_trie;
+static int common_trie_done_setup;
+
+static void init_common_trie(void)
+{
+        struct common_dir *p;
+
+        if (common_trie_done_setup)
+                return;
+
+        for (p = common_list; p->dirname; p++)
+                add_to_trie(&common_trie, p->dirname, p);
+
+        common_trie_done_setup = 1;
+}
 */
   
 } // namespace git
